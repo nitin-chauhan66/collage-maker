c62bdca98ab82abaa6cd826d437d49d06a242f71
nitinsfdc <nvch66@gmail.com>
2020-09-13 21:04:41 +0530

    Updates

---
+++ precache-manifest.cdb9da516d4391fd1954a5fecd8d31c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1771999d59fa972cadba3190c1ad90a3",
+    "url": "/collage-maker/index.html"
+  },
+  {
+    "revision": "8b552c20dc664a8e31b0",
+    "url": "/collage-maker/static/css/main.d4f470c0.chunk.css"
+  },
+  {
+    "revision": "a726d7b7af03f13f6861",
+    "url": "/collage-maker/static/js/2.d3701788.chunk.js"
+  },
+  {
+    "revision": "83a1b31f8e1b1ebe5257ec268fc4206d",
+    "url": "/collage-maker/static/js/2.d3701788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b552c20dc664a8e31b0",
+    "url": "/collage-maker/static/js/main.ac6a5763.chunk.js"
+  },
+  {
+    "revision": "29ba149276a489688739",
+    "url": "/collage-maker/static/js/runtime-main.a1106779.js"
+  }
+]);
